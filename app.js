7d3870f74f91513c6637814f8fa2f1406d5d7ef2
PHUICMT <icmtchannel@gmail.com>
2022-03-14 14:51:11 +0700

    ⚡️ handle express to receive video as blob

--- app.js
+++ app.js
@@ -1,45 +1,33 @@
-var videoReceiver = require("./services/video-receiver");
+var tools = require("./services/video-receiver");
 
-var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var cors = require("cors");
 
 var app = express();
 
-// view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "jade");
+var corsOptions = {
+  origin: ["http://localhost:3000"],
+};
 
-// app.use(logger("dev"));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("/", indexRouter);
-// app.use("/users", usersRouter);
-
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
+app.post("/upload-video", function (req, res) {
+  if (!req.body) {
+    console.log("No file received");
+    return res.status(400).json({ message: "Can't Accept" });
+  } else {
+    console.log("----------------req.body-------------");
+    console.log(req.body);
+    console.log("-------------------------------------");
+    tools.VideoReceiver(req.data);
+    return res.status(200);
+  }
+});
 
 app.listen(5000, () => console.log("Server Started!! 5000"));
-
-videoReceiver;
--- services/video-receiver.js
+++ services/video-receiver.js
@@ -1,27 +1,14 @@
-var fs = require("fs");
-var ss = require("socket.io-stream");
-var io = require("socket.io")({
-  allowEIO3: true, // false by default
-  transport: ["websocket"],
-  credentials: true,
-});
+var FileSaver = require("file-saver");
 
-// var socket = io.connect("localhost:5000");
-var stream = ss.createStream();
+const VideoReceiver = (req) => {
+  // console.log(req);
+  // let fileName = response.fileName;
+  // let blob = response.data;
+  // let type = response.type;
+  // console.log(fileName);
+  // console.log(blob);
+  // console.log(type);
+  // FileSaver.saveAs(blob, "../videos/" + fileName);
+};
 
-//Create a new file stream. You can write buffer data into this file.
-var outStream = fs.createWriteStream("./videos/out.mp4");
-
-io.on("filename", function (socket) {
-  ss(socket).emit("filename", stream);
-  stream.pipe(outStream);
-});
-
-// io.on("filename", function (socket) {
-//   console.log("filename");
-//   socket.on("blob", function (data) {
-//     //You will received buffer data from browser client and you will write buffer to client.
-//     console.log(data);
-//     outStream.write(data);
-//   });
-// });
+module.exports = { VideoReceiver };
